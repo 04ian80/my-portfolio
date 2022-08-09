e00037db5681e180bfd388d1c9d5b5d1b1a61473
Ain Chung <artnouveau0804@gmail.com>
2022-08-10 03:25:20 +0900

    [8] - icon color change when hovered
    icon에 마우스를 올리면 색깔이 그 로고에 맞게 변하게 설정해줌. (useReducer사용)
    나중에 transition으로 scale이나 아니면 color가 천천히 바뀌는 애니매이션 넣어줄 예정.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,10 +1,49 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 import * as C from '../style/SideBar.style';
 import * as palette from '../style/Variables';
 import { SVG } from '../icon/SVG';
 
+const initArg = {
+  githubColor: palette.subFontColor,
+  velogColor: palette.subFontColor,
+  gmailColor: palette.subFontColor,
+};
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case 'GITHUB':
+      return {
+        ...state,
+        githubColor: palette.githubColor,
+      };
+      break;
+    case 'VELOG':
+      return {
+        ...state,
+        velogColor: palette.velogColor,
+      };
+      break;
+    case 'GMAIL':
+      return {
+        ...state,
+        gmailColor: palette.gmailColor,
+      };
+      break;
+    case 'DEFAULT':
+      return {
+        ...state,
+        githubColor: palette.subFontColor,
+        velogColor: palette.subFontColor,
+        gmailColor: palette.subFontColor,
+      };
+      break;
+    default:
+      break;
+  }
+};
+
 function SideBar() {
-  const iconColor = palette.subFontColor;
+  const [state, dispatch] = useReducer(reducer, initArg);
 
   return (
     <C.Container>
@@ -26,22 +65,55 @@ function SideBar() {
           href='https://github.com/04ian80'
           rel='noopener noreferrer'
           target='_blank'
+          onMouseEnter={() => {
+            dispatch({ type: 'GITHUB' });
+          }}
+          onMouseLeave={() => {
+            dispatch({ type: 'DEFAULT' });
+          }}
         >
-          <SVG name='github' width='20px' height='20px' color={iconColor}></SVG>
+          <SVG
+            name='github'
+            width='20px'
+            height='20px'
+            color={state.githubColor}
+          ></SVG>
         </a>
         <a
           href='https://velog.io/@a_in'
           rel='noopener noreferrer'
           target='_blank'
+          onMouseEnter={() => {
+            dispatch({ type: 'VELOG' });
+          }}
+          onMouseLeave={() => {
+            dispatch({ type: 'DEFAULT' });
+          }}
         >
-          <SVG name='velog' width='20px' height='20px' color={iconColor}></SVG>
+          <SVG
+            name='velog'
+            width='20px'
+            height='20px'
+            color={state.velogColor}
+          ></SVG>
         </a>
         <a
           href='mailto:artnouveau0804@gmail.com'
           rel='noopener noreferrer'
           target='_blank'
+          onMouseEnter={() => {
+            dispatch({ type: 'GMAIL' });
+          }}
+          onMouseLeave={() => {
+            dispatch({ type: 'DEFAULT' });
+          }}
         >
-          <SVG name='gmail' width='20px' height='20px' color={iconColor}></SVG>
+          <SVG
+            name='gmail'
+            width='20px'
+            height='20px'
+            color={state.gmailColor}
+          ></SVG>
         </a>
       </C.ContactBox>
     </C.Container>
--- src/style/Variables.js
+++ src/style/Variables.js
@@ -1,3 +1,6 @@
 export const fontColor = '#1a1a1a';
 export const subFontColor = '#524f4a';
 export const bgColor = '#e5e3d7';
+export const githubColor = '#181717';
+export const velogColor = '#20C997';
+export const gmailColor = '#EA4335';
